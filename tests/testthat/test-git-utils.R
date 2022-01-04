c3b35a7a2a1f1d8e43888031f5008e83d7387998
Dyfan Jones <dyfan.jones@theverygroup.com>
2022-01-04 10:52:41 +0000

    feature: add extra mock tests

--- tests/testthat/test-git-utils.R
+++ tests/testthat/test-git-utils.R
@@ -162,7 +162,6 @@ test_that("test_git_clone_repo_with_username_password_no_2fa", {
   entry_point = "entry_point"
   source_dir = "source_dir"
   dependencies = list("foo", "dep_that_does_not_exist")
-  entry_point = "entry_point"
 
   env = c("current", "GIT_TERMINAL_PROMPT" = 0)
   mock_run = mock_fun()
@@ -186,6 +185,214 @@ test_that("test_git_clone_repo_with_username_password_no_2fa", {
       ))
       expect_equal(args[[2]], list("git", args = c("checkout", PRIVATE_BRANCH), wd = REPO_DIR))
       expect_equal(args[[3]], list("git", args = c("checkout", PRIVATE_COMMIT), wd = REPO_DIR))
+      expect_equal(ret$entry_point, "/tmp/repo_dir/entry_point")
+      expect_null(ret$source_dir)
+      expect_null(ret$dependencies)
+    }
+  )
+})
+
+test_that("test_git_clone_repo_with_token_no_2fa", {
+  git_config = list(
+    "repo"=PRIVATE_GIT_REPO,
+    "branch"=PRIVATE_BRANCH,
+    "commit"=PRIVATE_COMMIT,
+    "token"="my-token",
+    "2FA_enabled"=FALSE
+  )
+  entry_point = "entry_point"
+  source_dir = "source_dir"
+  dependencies = list("foo", "dep_that_does_not_exist")
+
+  env = c("current", "GIT_TERMINAL_PROMPT" = 0)
+  mock_run = mock_fun()
+  with_mock(
+    `fs::is_file` = mock_fun(TRUE),
+    `fs::file_temp` = mock_fun(REPO_DIR),
+    `processx::run` = mock_run,
+    {
+      ret = sagemaker.common::git_clone_repo(git_config=git_config, entry_point=entry_point)
+      args = mock_run(..return_value_all = T)
+      expect_equal(args[[1]], list(
+        "git",
+        args = c(
+          "clone",
+          "https://my-token@github.com/testAccount/private-repo.git",
+          REPO_DIR
+        ),
+        env = env
+      ))
+      expect_equal(args[[2]], list("git", args = c("checkout", PRIVATE_BRANCH), wd = REPO_DIR))
+      expect_equal(args[[3]], list("git", args = c("checkout", PRIVATE_COMMIT), wd = REPO_DIR))
+      expect_equal(ret$entry_point, "/tmp/repo_dir/entry_point")
+      expect_null(ret$source_dir)
+      expect_null(ret$dependencies)
+    }
+  )
+})
+
+test_that("test_git_clone_repo_with_token_2fa", {
+  git_config = list(
+    "repo"=PRIVATE_GIT_REPO,
+    "branch"=PRIVATE_BRANCH,
+    "commit"=PRIVATE_COMMIT,
+    "2FA_enabled"=TRUE,
+    "username"="username",
+    "token"="my-token"
+  )
+  entry_point = "entry_point"
+  source_dir = "source_dir"
+  dependencies = list("foo", "dep_that_does_not_exist")
+
+  env = c("current", "GIT_TERMINAL_PROMPT" = 0)
+  mock_run = mock_fun()
+  with_mock(
+    `fs::is_file` = mock_fun(TRUE),
+    `fs::file_temp` = mock_fun(REPO_DIR),
+    `processx::run` = mock_run,
+    {
+      expect_warning(
+        {ret = sagemaker.common::git_clone_repo(git_config=git_config, entry_point=entry_point)},
+        "Using token for authentication, other credentials will be ignored."
+      )
+      args = mock_run(..return_value_all = T)
+      expect_equal(args[[1]], list(
+        "git",
+        args = c(
+          "clone",
+          "https://my-token@github.com/testAccount/private-repo.git",
+          REPO_DIR
+        ),
+        env = env
+      ))
+      expect_equal(args[[2]], list("git", args = c("checkout", PRIVATE_BRANCH), wd = REPO_DIR))
+      expect_equal(args[[3]], list("git", args = c("checkout", PRIVATE_COMMIT), wd = REPO_DIR))
+      expect_equal(ret$entry_point, "/tmp/repo_dir/entry_point")
+      expect_null(ret$source_dir)
+      expect_null(ret$dependencies)
+    }
+  )
+})
+
+test_that("test_git_clone_repo_ssh", {
+  git_config = list("repo"=PRIVATE_GIT_REPO_SSH, "branch"=PRIVATE_BRANCH, "commit"=PRIVATE_COMMIT)
+  entry_point = "entry_point"
+
+  mock_chmod = mock_fun()
+  with_mock(
+    `processx::run` = mock_fun(),
+    `fs::file_chmod` = mock_chmod,
+    `fs::file_temp` = mock_fun(REPO_DIR),
+    `fs::is_file` = mock_fun(TRUE),
+    {
+      ret = sagemaker.common::git_clone_repo(git_config, entry_point)
+      expect_equal(mock_chmod(..return_value = T)$mode, "511")
+      expect_equal(ret$entry_point, "/tmp/repo_dir/entry_point")
+      expect_null(ret$source_dir)
+      expect_null(ret$dependencies)
+    }
+  )
+})
+
+test_that("test_git_clone_repo_with_token_no_2fa_unnecessary_creds_provided", {
+  git_config = list(
+    "repo"=PRIVATE_GIT_REPO,
+    "branch"=PRIVATE_BRANCH,
+    "commit"=PRIVATE_COMMIT,
+    "username"="username",
+    "password"="passw0rd!",
+    "token"="my-token"
+  )
+  entry_point = "entry_point"
+  env = c("current", "GIT_TERMINAL_PROMPT" = 0)
+  mock_run = mock_fun()
+  with_mock(
+    `processx::run` = mock_run,
+    `fs::file_temp` = mock_fun(REPO_DIR),
+    `fs::is_file` = mock_fun(TRUE),
+    {
+      expect_warning(
+        {ret = sagemaker.common::git_clone_repo(git_config, entry_point)},
+        "Using token for authentication, other credentials will be ignored."
+      )
+      args = mock_run(..return_value_all = T)
+      expect_equal(args[[1]],list(
+        "git",
+        args = c("clone", "https://my-token@github.com/testAccount/private-repo.git", REPO_DIR),
+        env = env
+      ))
+      expect_equal(args[[2]], list("git", args = c("checkout", PRIVATE_BRANCH), wd = REPO_DIR))
+      expect_equal(args[[3]], list("git", args = c("checkout", PRIVATE_COMMIT), wd = REPO_DIR))
+      expect_equal(ret$entry_point, "/tmp/repo_dir/entry_point")
+      expect_null(ret$source_dir)
+      expect_null(ret$dependencies)
+    }
+  )
+})
+
+test_that("test_git_clone_repo_with_token_2fa_unnecessary_creds_provided", {
+  git_config = list(
+    "repo"=PRIVATE_GIT_REPO,
+    "branch"=PRIVATE_BRANCH,
+    "commit"=PRIVATE_COMMIT,
+    "2FA_enabled"=TRUE,
+    "username"="username",
+    "token"="my-token"
+  )
+  entry_point = "entry_point"
+  env = c("current", "GIT_TERMINAL_PROMPT" = 0)
+  mock_run = mock_fun()
+  with_mock(
+    `processx::run` = mock_run,
+    `fs::file_temp` = mock_fun(REPO_DIR),
+    `fs::is_file` = mock_fun(TRUE),
+    {
+      expect_warning(
+        {ret = sagemaker.common::git_clone_repo(git_config, entry_point)},
+        "Using token for authentication, other credentials will be ignored."
+      )
+      args = mock_run(..return_value_all = T)
+      expect_equal(args[[1]],list(
+        "git",
+        args = c("clone", "https://my-token@github.com/testAccount/private-repo.git", REPO_DIR),
+        env = env
+      ))
+      expect_equal(args[[2]], list("git", args = c("checkout", PRIVATE_BRANCH), wd = REPO_DIR))
+      expect_equal(args[[3]], list("git", args = c("checkout", PRIVATE_COMMIT), wd = REPO_DIR))
+      expect_equal(ret$entry_point, "/tmp/repo_dir/entry_point")
+      expect_null(ret$source_dir)
+      expect_null(ret$dependencies)
+    }
+  )
+})
+
+test_that("test_git_clone_repo_codecommit_https_with_username_and_password", {
+  git_config = list(
+    "repo"=CODECOMMIT_REPO,
+    "branch"=CODECOMMIT_BRANCH,
+    "username"="username",
+    "password"="my-codecommit-password"
+  )
+  entry_point = "entry_point"
+  env = c("current", "GIT_TERMINAL_PROMPT" = 0)
+  mock_run = mock_fun()
+  with_mock(
+    `processx::run` = mock_run,
+    `fs::file_temp` = mock_fun(REPO_DIR),
+    `fs::is_file` = mock_fun(TRUE),
+    {
+      ret = sagemaker.common::git_clone_repo(git_config, entry_point)
+      args = mock_run(..return_value_all = T)
+      expect_equal(args[[1]],list(
+        "git",
+        args = c(
+          "clone",
+          "https://username:my-codecommit-password@git-codecommit.us-west-2.amazonaws.com/v1/repos/test-repo/",
+          REPO_DIR
+        ),
+        env = env
+      ))
+      expect_equal(args[[2]], list("git", args = c("checkout", CODECOMMIT_BRANCH), wd = REPO_DIR))
       expect_equal(ret$entry_point, "/tmp/repo_dir/entry_point")
       expect_null(ret$source_dir)
       expect_null(ret$dependencies)
